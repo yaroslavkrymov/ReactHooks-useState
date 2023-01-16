a1793620a6e23cdc567089859375ec36a134fd23
yaroslavkrymov <93751242+yaroslavkrymov@users.noreply.github.com>
2023-01-15 19:18:05 -0700

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+
+var count = 0;
+
+function increase() {}
+
+ReactDOM.render(<App />, document.getElementById("root"));
